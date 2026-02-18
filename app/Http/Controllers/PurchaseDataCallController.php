1c136afc02c65ff424f5ef0e1b9bd9798b42b856
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-02-18 17:46:58 +0500

    test

--- app/Http/Controllers/PurchaseDataCallController.php
+++ app/Http/Controllers/PurchaseDataCallController.php
@@ -3100,7 +3100,7 @@ $subitems = Subitem::where('subitem.status', 1)
         $id=$request->id;
        $vendorId = $request->vendor_id;
         $data = DB::Connection('mysql2')->table('currency')->where('to_type_id',$id)->select('*')->get();
-        $vendors = DB::Connection('mysql2')->table('supplier')->where('id',$vendorId)->select('*')->get();
+        $vendors = DB::Connection('mysql2')->table('supplier')->select('*')->get();
 
         $currencyOptions = '<option value="">Select Currency</option>';
 
@@ -3126,7 +3126,7 @@ $subitems = Subitem::where('subitem.status', 1)
         $id=$request->id;
    
         $data = DB::Connection('mysql2')->table('currency')->where('to_type_id',$id)->select('*')->get();
-        $vendors = DB::Connection('mysql2')->table('supplier')->where('to_type_id',$id)->select('*')->get();
+        $vendors = DB::Connection('mysql2')->table('supplier')->select('*')->get();
 
         $currencyOptions = '<option value="">Select Currency</option>';
 
